0929d5e6d287b4058faec92e1f4850c1c053d3da
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-05-24 20:20:11 +0200

    added main page

---
+++ script.js
@@ -0,0 +1,10 @@
+// menu opening and closing
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
